7935715a05971cb45b35bd55533fdc28b1e20e07
ATB7x <mahajanshamlit3@gmail.com>
2024-08-07 15:24:06 +0530

    All files upload

---
+++ src/ex_27072024/Lab002.java
@@ -0,0 +1,8 @@
+package ex_27072024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Good Morning All");
+        System.out.println("4+3");
+    }
+}
---
+++ src/ex_27072024/Lab003.java
@@ -0,0 +1,7 @@
+package ex_27072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Hieee");
+    }
+}
